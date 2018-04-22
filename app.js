da29b9d7eb8e569dcf81afbc728547de84108e4d
KeenanKunzelman <keekunz@gmail.com>
2018-04-22 18:50:44 -0400

    reafactored some code

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+
+
 const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode.js');
 
 const argv = yargs
   .options({
     a: {
       demand: true,
-      alias: 'Address',
+      alias: 'address',
       describe: 'Address to fetch weather for',
       string: true
     }
@@ -14,22 +18,14 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-console.log(argv);
 
-var encodedAddress = encodeURIComponent(argv.a)
 
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log("Unable to connect to Google API");
-  }else if (body.status === "ZERO_RESULTS") {
-    console.log("Unable to find that address");
-  }else if (body.status === "OK") {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Lattitude ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  }else{
+    console.log(JSON.stringify(results, undefined, 2));
   }
-
 });
+
+
